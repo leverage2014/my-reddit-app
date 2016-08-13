a831e4fa350bde4743f6891ae2fcae283bcca767
leverage2014 <myletter428@gmail.com>
2016-08-13 16:05:30 -0400

    Add cordova inAppBrowser

--- www/js/app.js
+++ www/js/app.js
@@ -42,6 +42,10 @@
       });
     }
 
+    $scope.openLink = function(url) {
+      window.open(url, '_blank');
+    }
+
     // $scope.$on('$stateChangeSuccess', function() {
     //   $scope.loadOlderStories();
     // });
@@ -51,14 +55,11 @@
   app.run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
       if (window.cordova && window.cordova.plugins.Keyboard) {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
         cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-        // Don't remove this line unless you know what you are doing. It stops the viewport
-        // from snapping when text inputs are focused. Ionic handles this internally for
-        // a much nicer keyboard experience.
         cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.cordova && window.cordova.InAppBrowser) {
+        window.open = window.cordova.InAppBrowser.open;
       }
       if (window.StatusBar) {
         StatusBar.styleDefault();
