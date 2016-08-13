70029c29da77045b662fcb79a1413d0bd00a48c9
leverage2014 <myletter428@gmail.com>
2016-08-13 15:39:59 -0400

    Add infinite-scroll and refresher

--- www/js/app.js
+++ www/js/app.js
@@ -6,14 +6,46 @@
   app.controller('RedditCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.stories = [];
 
-    $http.get('https://www.reddit.com/r/Android/new/.json')
-      .success(function(response) {
-        // console.log('response', response);
-        angular.forEach(response.data.children, function(child) {
-          // console.log(child.data);
-          $scope.stories.push(child.data);
+    function loadStories(params, callback) {
+      $http.get('https://www.reddit.com/r/funny/new/.json', {
+          params: params
+        })
+        .success(function(response) {
+          var stories = [];
+          angular.forEach(response.data.children, function(child) {
+            stories.push(child.data);
+          });
+          callback(stories);
         });
+    }
+
+    $scope.loadNewerStories = function() {
+      console.log('load new stories!');
+      var params = {
+        'before': $scope.stories[0].name
+      };
+      loadStories(params, function(newerStories) {
+        $scope.stories = newerStories.concat($scope.stories);
+        $scope.$broadcast('scroll.refreshComplete');
       });
+    }
+
+    $scope.loadOlderStories = function() {
+      console.log('load older stories');
+      var params = {};
+      if ($scope.stories.length > 0) {
+        params['after'] = $scope.stories[$scope.stories.length - 1].name;
+      }
+      loadStories(params, function(olderStories) {
+        $scope.stories = $scope.stories.concat(olderStories);
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+      });
+    }
+
+    // $scope.$on('$stateChangeSuccess', function() {
+    //   $scope.loadOlderStories();
+    // });
+
   }]);
 
   app.run(function($ionicPlatform) {
